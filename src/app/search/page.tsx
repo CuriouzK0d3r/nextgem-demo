e133f43285aaab2d0959260595d33ec707edb067
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2024-01-18 17:47:56 +0200

    search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -165,10 +165,40 @@ function LoginPage() {
         console.error("Error:", error);
       });
   }
-  // Add
+
+  const TextInputComponent = ({ label, type, required, ref }: any) => {
+    return (
+      <div className="flex flex-row mt-4">
+        <label className="mb-2 ttext-based text-gray-900 mr-4">{label}:</label>
+        <input
+          className="border border-gray-400 p-2 rounded-lg w-3/4 appearance-none focus:outline-none focus:border-gray-500"
+          type={type}
+          id={label}
+          name={label}
+          placeholder={"Insert text"}
+          required={required}
+        // ref={ref}
+        />
+      </div>
+    );
+  }
+  const SelectInputComponent = ({ label, values, required, ref }: any) => {
+    return (
+      <div className="flex flex-row mt-4">
+        <label className="mb-2 text-base text-gray-900 mr-4">{label}</label>
+        <select
+          className="text-sm rounded-lg  block p-2.5   w-3/4 dark:border-gray-600">
+          {values.map((value: any) => (
+            <option value={value}>{value}</option>
+          ))
+          }
+        </select>
+      </div>
+    );
+  }
 
   return (
-    <div className='h-full w-full'>
+    <div className='h-full w-full '>
       <header>
         <nav className="bg-niki-blue bg-opacity-90 border-gray-200 px-4 lg:px-6 py-4">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-[100rem]">
@@ -179,27 +209,26 @@ function LoginPage() {
                 alt="logo-img"
                 id="logo-img"
               />
-              {/* <!-- <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Search</span> --> */}
             </a>
-            {/* <a href="/upload.html" className="mt-2" style="text-decoration: underline;">Upload</a> */}
-
+            <div>
+              <a href="" className="text-base font-medium text-white hover:text-gray-200"> </a>
+            </div>
           </div>
         </nav>
       </header>
-      <div className="container w-3/4">
-        <h3 className='mb-2 text-6xl .title-color'>Search Scientific Catalogue</h3>
-        <div className='form-container w-full'>
-
+      <div className="w-full mt-16 h-full ">
+        <h3 className='mb-4 text-5xl .title-color text-center mx-auto'>Search Scientific Catalogue</h3>
+        <div className='form-container w-3/4 mx-auto'>
           <form className='flex-1 w-3/4 mx-auto mb-10' >
             <div className="flex">
               {/* <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label> */}
-              <Menu as="div" className="relative flex-none text-left w-[150px]">
+              <Menu as="div" className="relative flex-none text-left w-[150px] h-[74px] bottom-[2px]">
                 <div>
-                  <Menu.Button style={{ borderWidth: "2px",  borderTopLeftRadius: "8px" }} id="dropdown-button" data-dropdown-toggle="dropdown"
-                    className=" w-[150px]  h-[70px] flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center 
+                  <Menu.Button style={{ borderWidth: "2px", borderTopLeftRadius: "8px" }} id="dropdown-button" data-dropdown-toggle="dropdown"
+                    className=" w-[150px]  h-[74px] flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center 
             text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none
              focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700
-              dark:text-white dark:border-gray-600 pl-9">
+              dark:text-white dark:border-gray-600 ">
                     { }
                     <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
                   </Menu.Button>
@@ -310,77 +339,55 @@ function LoginPage() {
                   onChange={(e) => { }}
                   type="search" id="search-dropdown" className="w-full h-[70px] block p-2.5 z-20 text-sm text-gray-900 
                 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 
-                dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                dark:bg-gray-700 dark:border-l-gray-700 mb-0  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
                 dark:focus:border-blue-500" placeholder="Search Titles, DOIs, Authors, Subjects" required />
                 <div className='h-full'>
-                  <button style={{ borderWidth: "2px", borderColor: "#6359E1", borderTopLeftRadius: "8px" }} type="submit" className="w-[100px] absolute top-0 right-0 pl-9 text-sm font-medium h-[70px] text-white bg-niki-blue rounded-r-lg border border-blue-500 hover:bg-niki-blue focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-700">
+                  <button style={{ borderWidth: "2px", borderColor: "#6359E1" }} type="submit" className="w-[100px] absolute top-0 right-0 pl-9 text-sm font-medium h-[70px] text-white bg-niki-blue rounded-r-lg border border-blue-500 hover:bg-niki-blue focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-700">
                     <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg>
                     <span className="sr-only">Search</span>
                   </button>
                 </div>
-
               </div>
             </div>
           </form>
           <form className='mt-4' id="searchForm">
-            <h5 className='text-center text-xl mb-10'>
+            {/* <h5 className='text-center text-xl mb-10'>
               Search Terms:
-            </h5>
-            <div className="grid gap-6 mb-6 md:grid-cols-2 ">
+            </h5> */}
+            <div className="grid gap-8 mb-6 md:grid-cols-2 ">
               <div>
-                <span className='underline text-lg mb-10'>
+                <div className='underline text-md mb-8'>
                   Description
-                </span>
+                </div>
                 <div>
-                  <div className="flex mb-4">
-                    <label className="block mb-2  font-medium text-gray-900 mr-5">Type of Study</label>
-                    <select
-                      className="text-sm rounded-lg  block p-2.5  w-2/4 dark:border-gray-600">
-                      <option value="">--</option>
-                      <option value="exVivo">exVivo</option>
-                      <option value="exposureAssessment">exposureAssessment</option>
-                      <option value="humanStudies">humanStudies</option>
-                      <option value="inVitro">inVitro</option>
-                      <option value="inVivo">inVivo</option>
-                      <option value="riskAssessment">riskAssessment</option>
-                      <option value="simulation">simulation</option>
-                    </select>
-                  </div>
-                  <div className="flex ">
-                    <label className="block mb-2  font-medium text-gray-900 mr-3">Type of Output</label>
-                    <select
-                      className="text-sm rounded-lg  block w-2/4 p-2.5  dark:border-gray-600">
-                      <option value="">--</option>
-                      <option value="audio">audio</option>
-                      <option value="codebook">codebook</option>
-                      <option value="dataset">dataset</option>
-                      <option value="deliverable">deliverable</option>
-                      <option value="image">image</option>
-                      <option value="poster">poster</option>
-                      <option value="presentation">presentation</option>
-                      <option value="publication">publication</option>
-                      <option value="report">report</option>
-                      <option value="software">software</option>
-                      <option value="video">video</option>
-                    </select>
-                  </div>
+                  <TextInputComponent label="Title" type="text" required={false} />
+                  <TextInputComponent label="Authors" type="text" required={false} />
+                  <SelectInputComponent label="Type of Study" values={["--", "exVivo", "exposureAssessment", "humanStudies", "inVitro", "inVivo", "riskAssesment", "simulation"]} required={false} />
+                  <SelectInputComponent label="Type of Output" values={["--", "audio", "codebook", "dataset", "deliverable", "image", "poster", "presentation", "publication", "report", "software", "video"]} required={false} />
+                  <SelectInputComponent label="Topics" values={["--", ""]} required={false} />
+
+                  <TextInputComponent label="Publication Year" type="text" required={false} />
+                  <SelectInputComponent label="Privacy Level" values={["--", ""]} required={false} />
+
+                  <TextInputComponent label="Identifier" type="text" required={false} />
                 </div>
               </div>
               <div>
-                <span className='underline'>
+                <div className='underline text-md mb-8'>
                   Indicative Terms
-                </span>
-              </div>
-              <div className="flex">
-                <label className="block mb-2  font-medium text-gray-900 mr-10">Modulation</label>
-                <select
-                  className=" rounded-lg  block w-2/4 p-2.5  dark:border-gray-600 ">
-                  <option value="">--</option>
-                  <option value="NR">NR</option>
-                  <option value="noModulation">No Modulation</option>
-                </select>
+                </div>
+                <SelectInputComponent label="Frequency Ranges" values={["--", ""]} required={false} />
+                <SelectInputComponent label="Modulation" values={["--", "NR", "No Modulation"]} required={false} />
+                <SelectInputComponent label="Exposure Conditions" values={["--", ""]} required={false} />
+                <SelectInputComponent label="Exposure Sources" values={["--", ""]} required={false} />
+                <SelectInputComponent label="Environment" values={["--", ""]} required={false} />
+                <SelectInputComponent label="Microenvironment" values={["--", ""]} required={false} />
+                <SelectInputComponent label="Biological Model" values={["--", ""]} required={false} />
+                <SelectInputComponent label="BioSpecific Endpoints" values={["--", ""]} required={false} />
+                <SelectInputComponent label="Methods" values={["--", ""]} required={false} />
+
               </div>
               {/* <div>
                 <label className="block mb-2  font-medium text-gray-900 ">Institution</label>
@@ -454,6 +461,19 @@ function LoginPage() {
               <input type="submit" id="searchBtn" className="btn font-bold w-30" value="Search" />
             </div>
           </form>
+        </div>
+        <div className='flex flex-row mx-auto w-full mt-10 mb-20'>
+          <div className='w-1/2 flex mx-auto items-center'>
+          <button className={""} onClick={() => { }}>
+            <img width={100} src="./NextGEM_Button.svg" alt="NextGEM" className="mr-[9px] chosen-source" />
+          </button>
+          <img width={100} src="./CLUE-H_Button.svg" alt="CLUE-H" className="mr-[0px]" />
+          <img width={115} src="./EMF-portal_Button.svg" alt="EMF-Portal" className="mr-[2px]" />
+          <img width={100} src="./Zenodo_Button.svg" alt="Zenodo" className="mr-[9px]" />
+          <img width={100} src="./EHDS_Button.svg" alt="EHDS" className="mr-[8px]" />
+          <img width={105} src="./Dataverse.svg" alt="Dataverse" className="mr-[8px]" />
+          <img width={100} src="./YODA.svg" alt="Yoda" className="" />
+          </div>
         </div>
         <Card className="h-full w-full overflow-scroll hidden">
           <table className="w-full min-w-max table-auto text-left">
