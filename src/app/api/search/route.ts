be3b8bb4c5e497acccec38fd7ee7f7b5dd3e6c9f
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2024-01-31 18:19:35 +0200

    search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -5,19 +5,22 @@ export async function POST(req: Request, res: NextApiResponse) {
         res.status(405).json({ message: 'Method Not Allowed' });
         return;
     }
-
     let apiEndpoint = "https://139.91.58.16/metadata/records?";
     const params = await req.json();
 
-    const keys = Object.keys(params);
+    const keys = Object.keys(params["formData"]);
+    const formData = params["formData"];
+
     for (let i = 0; i < keys.length; i++) {
         const key = keys[i];
-        const value = params[key];
+        const value = formData[key];
         if (value === '') {
             continue
         }
         apiEndpoint += `${key}=${value}&`;
     }
+
+    console.log(apiEndpoint)
 
     const response = await fetch(apiEndpoint, {
         method: 'GET',
@@ -28,6 +31,7 @@ export async function POST(req: Request, res: NextApiResponse) {
             'Access-Control-Allow-Headers': 'Content-Type, X-CSRF-TOKEN',
         }
     })
+
     const responseJSON = await response.json();
 
     return Response.json({ searchResults: responseJSON });
