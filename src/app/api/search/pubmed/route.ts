237853d95a1de879552b70e5443536355402b0df
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2024-03-29 18:27:42 +0200

    pubmed

--- src/app/api/search/pubmed/route.ts
+++ src/app/api/search/pubmed/route.ts
@@ -1,8 +1,21 @@
 import axios from 'axios';
+import xmljs from 'xml-js';
 
-const getPubmedData = async (id: number) => {
-    const response = await axios.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=${id}&retmode=json`);
-    return await response.data.result[id];
+const getPubmedData = async (ids: number[]) => {
+    console.log(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${ids[0]}&rettype=xml&retmode=json`)
+    const response = await axios.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${ids.join(',')}&rettype=xml&retmode=json`);
+    return await response.data;
+}
+
+let _requestsMade: any[] = [];
+
+const exceedRateLimit = () => {
+    let now = new Date();
+    let oneSecondAgo = new Date(now.getTime() - 1000); // Subtracting 1000 milliseconds (1 second) from the current time
+
+    let filteredRequests = _requestsMade.filter(requestTime => requestTime > oneSecondAgo);
+    // Return whether we've made more requests in the last second than the rate limit
+    return filteredRequests.length > 2;
 }
 
 export async function POST(req: Request) {
@@ -10,18 +23,22 @@ export async function POST(req: Request) {
         let data = await req.json();
 
         const response = await axios.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=${data.query}&retmode=json&retstart=0&retmax=20`);
-
+        _requestsMade.push(new Date());
         let returnedIds = await response.data.esearchresult.idlist;
         // console.log(returnedIds)
-        let returnedObj: any[] = [];
-        for (let i = 0; i < returnedIds.length; i++) {
-            console.log(returnedIds[i])
-            let pubmedData = await getPubmedData(returnedIds[i]);
-            console.log(pubmedData)
-            returnedObj.push(pubmedData);
-        }
 
-        console.log(returnedObj);
+        let returnedObj: any[] = [];
+        // for (let i = 0; i < returnedIds.length; i++) {
+            while (exceedRateLimit()) {
+                // This loop will continue indefinitely until the rate limit is no longer exceeded
+            }
+
+            let pubmedData = await getPubmedData(returnedIds);
+            console.log(xmljs.xml2json(pubmedData))
+            // _requestsMade.push(new Date());
+
+            returnedObj.push(pubmedData);
+        // }
 
         return Response.json(JSON.stringify(returnedObj));
     } catch (err) {
