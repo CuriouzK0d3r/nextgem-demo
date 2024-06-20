124f57594c28b5702c6c5e9dedda79a218b42a36
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2024-06-20 13:45:08 +0000

    gitpod

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -26,6 +26,6 @@ describe("Scientists page", () => {
     it("renders /scientists", async () => {
         await act( async () => render(<Home/>));
 
-        expect(2 == 2);
+        expect(2).toBe(2);
     });
 });
